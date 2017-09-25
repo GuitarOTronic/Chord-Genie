d08d77aeaaebcb88805b5824e2f5215fde1e3108
Sean Obringer <SeanObringer90@gmail.com>
2017-09-25 16:42:52 -0700

    added musical alphabet and findRootIndex function

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,4 @@
+//*********   Populate picked root and chord type  ***************
 var root = document.getElementsByClassName('note')
 var displaySelectedChord = document.getElementById('selectedChord')
 var selectedRoot = ''
@@ -7,12 +8,12 @@ for (var i = 0; i < root.length; i++) {
   root[i].addEventListener('click', function(event) {
     selectedRoot = event.target.innerText
     displaySelectedChord.innerText = selectedRoot
-    console.log(selectedRoot);
+
   })
 }
 
 var chordType = document.getElementsByClassName('chord')
-var chordCount = 0
+
 
 for (var i = 0; i < chordType.length; i++) {
   chordType[i].addEventListener('click', function(event) {
@@ -20,5 +21,20 @@ for (var i = 0; i < chordType.length; i++) {
     displaySelectedChord.innerText = selectedRoot + ' ' + event.target.innerText
     selectedChordType = event.target.innerText;
     console.log(selectedChordType);
+    majorChord(selectedRoot)
   })
+}
+
+
+// ************   get chord tones   ***********************
+var alphabet = ['A', 'A#/Bb', 'B', 'C', 'C#/Db', 'D', 'D#/Eb', 'E', 'F',
+  'F#/Gb', 'G', 'G#/Ab', 'A', 'A#/Bb', 'B', 'C', 'C#/Db', 'D', 'D#/Eb', 'E', 'F',
+  'F#/Gb', 'G',
+]
+var indexOfRoot
+
+function majorChord(root) {
+  indexOfRoot = alphabet.indexOf(root)
+  console.log(indexOfRoot);
+
 }
