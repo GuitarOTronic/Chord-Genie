b60ff122978c6c499b2ce28e3e6129113c6e0ebb
Sean Obringer <SeanObringer90@gmail.com>
2017-09-27 14:17:31 -0700

    major and minor chord notes appear on diagram, chord tones appear in their box

--- scripts/main.js
+++ scripts/main.js
@@ -13,28 +13,85 @@ for (var i = 0; i < root.length; i++) {
 }
 
 var chordType = document.getElementsByClassName('chord')
-
-
+//*********  Find index of root    *********
 for (var i = 0; i < chordType.length; i++) {
   chordType[i].addEventListener('click', function(event) {
 
     displaySelectedChord.innerText = selectedRoot + ' ' + event.target.innerText
     selectedChordType = event.target.innerText;
-    console.log(selectedChordType);
-    findIndexOfRoot(selectedRoot)
+
+    findIndexOfRoot(selectedRoot);
+
   })
 }
-
-
 // ************   get chord tones   ***********************
 var alphabet = ['A', 'A#/Bb', 'B', 'C', 'C#/Db', 'D', 'D#/Eb', 'E', 'F',
   'F#/Gb', 'G', 'G#/Ab', 'A', 'A#/Bb', 'B', 'C', 'C#/Db', 'D', 'D#/Eb', 'E', 'F',
   'F#/Gb', 'G',
 ]
 var indexOfRoot
+var chordTones = []
 
 function findIndexOfRoot(root) {
   indexOfRoot = alphabet.indexOf(root)
-  console.log(indexOfRoot);
 
 }
+//**********  Major chord   ***********
+var majChord = document.getElementById('major')
+
+majChord.addEventListener('click', function() {
+  majorChord(alphabet)
+})
+
+function majorChord(alph) {
+  if (chordTones.length > 0) {
+    chordTones = []
+  }
+  chordTones.push(alph[indexOfRoot].toLowerCase())
+
+  chordTones.push(alph[indexOfRoot + 4].toLowerCase())
+  chordTones.push(alph[indexOfRoot + 7].toLowerCase())
+
+  displayCT.innerText = chordTones.join('-').toUpperCase()
+
+}
+//*************   Minor Chord    *******************
+
+var minChord = document.getElementById('minor')
+var displayCT = document.getElementById('displayCT')
+minChord.addEventListener('click', function() {
+  minorChord(alphabet)
+  displayCT.innerText = chordTones.join('-')
+
+})
+
+function minorChord(alph) {
+  if (chordTones.length > 0) {
+    chordTones = []
+  }
+  chordTones.push(alph[indexOfRoot].toLowerCase())
+  chordTones.push(alph[indexOfRoot + 3].toLowerCase())
+  chordTones.push(alph[indexOfRoot + 7].toLowerCase())
+
+}
+//*************  Chord Tone Display Button  ********
+var lowerCaseCT = chordTones.join('')
+var noteCircle = document.getElementsByClassName('circle')
+
+var showChord = document.getElementById('showChord')
+showChord.addEventListener('click', function() {
+  for (var i = 0; i < noteCircle.length; i++) {
+    noteCircle[i].style.visibility = 'hidden'
+  }
+
+  // populate major chord diagram
+
+  for (var i = 0; i < chordTones.length; i++) {
+    for (var j = 0; j < noteCircle.length; j++) {
+
+      if (noteCircle[j].classList[1] === chordTones[i]) {
+        noteCircle[j].style.visibility = 'visible'
+      }
+    }
+  }
+})
