0c6305676b796f956ffb65f1cd276a969ba4c75a
Sean Obringer <SeanObringer90@gmail.com>
2017-10-01 22:56:00 -0700

    added html page for game page. added/adding play button and answer form.

---
+++ scripts/game.js
@@ -0,0 +1,6 @@
+var listenGame = document.getElementById('listenGame')
+var audioFiles = document.getElementsByClassName("audioFiles")
+listenGame.addEventListener('click', function() {
+  var audioFiles = document.getElementsByClassName("audioFiles")
+  console.log(audioFiles);
+})
