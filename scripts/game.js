3c575e00225b446d3cb3d0825176339d984c4705
Sean Obringer <SeanObringer90@gmail.com>
2017-10-02 13:21:52 -0700

    GAME IS WORKING!!!! with randomly picked chords from the audio database.

--- scripts/game.js
+++ scripts/game.js
@@ -1,6 +1,82 @@
-var listenGame = document.getElementById('listenGame')
+var playChord = document.getElementById('playChord')
 var audioFiles = document.getElementsByClassName("audioFiles")
-listenGame.addEventListener('click', function() {
-  var audioFiles = document.getElementsByClassName("audioFiles")
-  console.log(audioFiles);
+
+var audioObject = {
+  amaj: 'assets/converted/amaj.mp3',
+  adom7: "assets/converted/adom7.mp3",
+  amaj7: "assets/converted/amaj7.mp3",
+  amin: "assets/converted/amin.mp3",
+  amin7: "assets/converted/amin7.mp3",
+  asmaj: "assets/converted/asmaj.mp3",
+  asmaj7: "assets/converted/asmaj7.mp3",
+  asmin7: "assets/converted/asmin.mp3",
+  asmin7: "assets/converted/asmin7.mp3"
+}
+var randomIndex
+
+var randomNum = function() {
+  min = 0;
+  max = Object.keys(audioObject).length;
+  randomIndex = Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+var answer
+
+//checks for type of chord
+var randomChordType = function() {
+  if (Object.keys(audioObject)[randomIndex].includes('s')) {
+    answer = Object.keys(audioObject)[randomIndex].slice(2)
+
+  } else {
+    answer = Object.keys(audioObject)[randomIndex].slice(1)
+
+  }
+}
+//Object.keys(audioObject)[randomIndex]
+
+
+//plays quiz chord.
+//gets chord from the randomIndex
+playChord.addEventListener('click', function() {
+
+  if (randomIndex === undefined) {
+    randomNum()
+    randomChordType()
+  }
+
+  console.log(answer);
+
+  var getChordAudio = document.createElement('audio')
+  getChordAudio.src = audioObject[Object.keys(audioObject)[randomIndex]]
+  getChordAudio.play()
+})
+
+var response = document.getElementById('response')
+//check for correct answer
+var userAnswer = document.getElementById('gameChoicesForm')
+userAnswer.addEventListener('submit', function(event) {
+  event.preventDefault()
+
+  if (document.getElementById('maj').checked && answer === 'maj') {
+    response.innerText = 'You are Correct! Move on to the next chord!'
+    randomIndex = undefined;
+  } else if (document.getElementById('min').checked && answer === 'min') {
+    response.innerText = 'You are Correct! Move on to the next chord!'
+    randomIndex = undefined;
+  } else if (document.getElementById('min7').checked && answer === 'min7') {
+    response.innerText = 'You are Correct! Move on to the next chord!'
+    randomIndex = undefined;
+  } else if (document.getElementById('dom7').checked && answer === 'dom7') {
+    response.innerText = 'You are Correct! Move on to the next chord!'
+    randomIndex = undefined;
+  } else if (document.getElementById('maj7').checked && answer === 'maj7') {
+    response.innerText = 'You are Correct! Move on to the next chord!'
+    randomIndex = undefined;
+  } else(
+    response.innerText = 'WRONG'
+  )
+
+  if (answer === userAnswer.value) {
+
+  }
 })
