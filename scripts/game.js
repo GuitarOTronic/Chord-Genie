cef2bb03f887ee6eb69cc00de5728e297d6a41e5
Sean Obringer <SeanObringer90@gmail.com>
2017-10-05 09:48:17 -0700

    typeo

--- scripts/game.js
+++ scripts/game.js
@@ -12,10 +12,70 @@ var audioObject = {
   amaj7: "assets/converted/amaj7.mp3",
   amin: "assets/converted/amin.mp3",
   amin7: "assets/converted/amin7.mp3",
+
   asmaj: "assets/converted/asmaj.mp3",
   asmaj7: "assets/converted/asmaj7.mp3",
   asmin7: "assets/converted/asmin.mp3",
-  asmin7: "assets/converted/asmin7.mp3"
+  asmin7: "assets/converted/asmin7.mp3",
+
+  bmaj: "assets/converted/bmaj.mp3",
+  bmaj7: "assets/converted/bmaj7.mp3",
+  bmin: "assets/converted/bmin.mp3",
+  bmin7: "assets/converted/bmin7.mp3",
+
+  cmaj: "assets/converted/cmaj.mp3",
+  cdom7: "assets/converted/cdom7.mp3",
+  cmaj7: "assets/converted/cmaj7.mp3",
+  cmin: "assets/converted/cmin.mp3",
+  cmin7: "assets/converted/cmin7.mp3",
+
+  csmaj: "assets/converted/csmaj.mp3",
+  csdom7: "assets/converted/csdom7.mp3",
+  csmaj7: "assets/converted/csmaj7.mp3",
+  csmin: "assets/converted/csmin.mp3",
+  csmin7: "assets/converted/csmin7.mp3",
+
+  dmaj: "assets/converted/dmaj.mp3",
+  ddom7: "assets/converted/ddom7.mp3",
+  dmaj7: "assets/converted/dmaj7.mp3",
+  dmin: "assets/converted/dmin.mp3",
+  dmin7: "assets/converted/dmin7.mp3",
+
+  dsmaj: "assets/converted/dsmaj.mp3",
+  dsdom7: "assets/converted/dsdom7.mp3",
+  dsmaj7: "assets/converted/dsmaj7.mp3",
+  dsmin: "assets/converted/dsmin.mp3",
+  dsmin7: "assets/converted/dsmin7.mp3",
+
+  emaj: "assets/converted/emaj.mp3",
+  edom7: "assets/converted/edom7.mp3",
+  emaj7: "assets/converted/emaj7.mp3",
+  emin: "assets/converted/emin.mp3",
+  emin7: "assets/converted/emin7.mp3",
+
+  fmaj: "assets/converted/fmaj.mp3",
+  fdom7: "assets/converted/fdom7.mp3",
+  fmaj7: "assets/converted/fmaj7.mp3",
+  fmin: "assets/converted/fmin.mp3",
+  fmin7: "assets/converted/fmin7.mp3",
+
+  fsmaj: "assets/converted/fsmaj.mp3",
+  fsdom7: "assets/converted/fsdom7.mp3",
+  fsmaj7: "assets/converted/fsmaj7.mp3",
+  fsmin: "assets/converted/fsmin.mp3",
+  fsmin7: "assets/converted/fsmin7.mp3",
+
+  gmaj: "assets/converted/gmaj.mp3",
+  gdom7: "assets/converted/gdom7.mp3",
+  gmaj7: "assets/converted/gmaj7.mp3",
+  gmin: "assets/converted/gmin.mp3",
+  gmin7: "assets/converted/gmin7.mp3",
+
+  gsmaj: "assets/converted/gsmaj.mp3",
+  gsdom7: "assets/converted/gsdom7.mp3",
+  gsmaj7: "assets/converted/gsmaj7.mp3",
+  gsmin: "assets/converted/gsmin.mp3",
+  gsmin7: "assets/converted/gsmin7.mp3"
 }
 var randomIndex
 
@@ -127,12 +187,13 @@ userAnswer.addEventListener('submit', function(event) {
     //sets local storage to best streak ever
     if (!localStorage.getItem('bestStreak')) {
       localStorage.setItem("bestStreak", JSON.stringify(document.getElementById('currentStreak').innerText))
-      var streaky = JSON.parse(localStorage.getItem("bestStreak"))
+      //var streaky = JSON.parse(localStorage.getItem("bestStreak"))
 
       //sets new high streak
     } else if (Number(JSON.parse(localStorage.getItem('bestStreak'))) < document.getElementById('currentStreak').innerText) {
       localStorage.setItem("bestStreak", JSON.stringify(document.getElementById('currentStreak').innerText))
       document.getElementById('bestStreak').innerText = document.getElementById('currentStreak').innerText
+      alert('Great job! You beat your high score!')
     }
 
 
